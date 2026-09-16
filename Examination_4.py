3b0b7e679bc5dc4dd913fd03b5f2065a34072d13
27926-coder <27926@ud.ac.th>
2026-09-16 11:18:50 +0700

    Update Examination_4.py

--- Examination_4.py
+++ Examination_4.py
@@ -9,9 +9,9 @@
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
 score = int(input())
 score = int(input())
-if score =>=80 = A
-   score =>=70 = B
-   score =>=60 = C
-   score =>=50 = D
+if score = >=80 = A
+   score = >=70 = B
+   score = >=60 = C
+   score = >=50 = D
 elif: 
    print( >=80 , >=70 , >=60 , >=50 )
